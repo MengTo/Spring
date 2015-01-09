48c2a7493adf417cd12a0fbf9dcd4b2a5bf58fd6
Meng To <shadownessguy@gmail.com>
2015-01-09 15:31:52 +0700

    delegates for table cell and button animations.

---
+++ KeyboardAvoidingScrollView.swift
@@ -0,0 +1,15 @@
+//
+//  KeyboardAvoidingScrollView.swift
+//  DesignerNewsApp
+//
+//  Created by Meng To on 2015-01-09.
+//  Copyright (c) 2015 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardAvoidingScrollView: UIScrollView, UITextFieldDelegate, UITextViewDelegate {
+
+    
+
+}
--- Misc.swift
+++ Misc.swift
@@ -86,10 +86,16 @@ func UIColorFromRGB(rgbValue: UInt) -> UIColor {
     )
 }
 
-func formatDate(date: NSDate, format: String) -> String {
+func stringFromDate(date: NSDate, format: String) -> String {
     var dateFormatter = NSDateFormatter()
     dateFormatter.dateFormat = format
     return dateFormatter.stringFromDate(date)
+}
+
+func dateFromString(date: String, format: String) -> NSDate {
+    var dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = format
+    return dateFormatter.dateFromString(date)!
 }
 
 func randomStringWithLength (len : Int) -> NSString {
@@ -105,4 +111,68 @@ func randomStringWithLength (len : Int) -> NSString {
     }
     
     return randomString
+}
+
+func timeAgoSinceDate(date:NSDate, numericDates:Bool) -> String {
+    let calendar = NSCalendar.currentCalendar()
+    let unitFlags = NSCalendarUnit.CalendarUnitMinute | NSCalendarUnit.CalendarUnitHour | NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitWeekOfYear | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear | NSCalendarUnit.CalendarUnitSecond
+    let now = NSDate()
+    let earliest = now.earlierDate(date)
+    let latest = (earliest == now) ? date : now
+    let components:NSDateComponents = calendar.components(unitFlags, fromDate: earliest, toDate: latest, options: nil)
+    
+    if (components.year >= 2) {
+        return "\(components.year)y"
+    } else if (components.year >= 1){
+        if (numericDates){
+            return "1y"
+        } else {
+            return "1y"
+        }
+    } else if (components.month >= 2) {
+        return "\(components.month)"
+    } else if (components.month >= 1){
+        if (numericDates){
+            return "1M"
+        } else {
+            return "1M"
+        }
+    } else if (components.weekOfYear >= 2) {
+        return "\(components.weekOfYear)w"
+    } else if (components.weekOfYear >= 1){
+        if (numericDates){
+            return "1w"
+        } else {
+            return "1w"
+        }
+    } else if (components.day >= 2) {
+        return "\(components.day)d"
+    } else if (components.day >= 1){
+        if (numericDates){
+            return "1d"
+        } else {
+            return "1d"
+        }
+    } else if (components.hour >= 2) {
+        return "\(components.hour)h"
+    } else if (components.hour >= 1){
+        if (numericDates){
+            return "1h"
+        } else {
+            return "1h"
+        }
+    } else if (components.minute >= 2) {
+        return "\(components.minute)m"
+    } else if (components.minute >= 1){
+        if (numericDates){
+            return "1m"
+        } else {
+            return "1m"
+        }
+    } else if (components.second >= 3) {
+        return "\(components.second)s"
+    } else {
+        return "now"
+    }
+    
 }
