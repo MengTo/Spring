50b0b08f3f1c2c7b85101028c13ab1d1dac6dc9d
Jaison Vieira <jaisonnvieira@gmail.com>
2015-06-28 04:51:47 -0300

    Make AutoTextView class public
    Now it can be used by importing Spring (with CocoaPods).

--- Spring/AutoTextView.swift
+++ Spring/AutoTextView.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class AutoTextView: UITextView {
-    override func intrinsicContentSize() -> CGSize {
+public class AutoTextView: UITextView {
+    override public func intrinsicContentSize() -> CGSize {
         var size = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
         size.width = self.frame.size.width
         if text.length == 0 {
