3f9febe52ba261f34a864ad1eaffa30d6e9442b9
André Schneider <hello@andreschneider.me>
2015-07-14 12:56:05 +0200

    Add AnimationPreset enumeration.

--- Spring/Spring.swift
+++ Spring/Spring.swift
@@ -102,6 +102,36 @@ public class Spring : NSObject {
     private var transform : CGAffineTransform { get { return view.transform } set { view.transform = newValue }}
     private var alpha: CGFloat { get { return view.alpha } set { view.alpha = newValue } }
 
+    enum AnimationPreset: String {
+        case SlideLeft = "slideLeft"
+        case SlideRight = "slideRight"
+        case SlideDown = "slideDown"
+        case SlideUp = "slideUp"
+        case SqueezeLeft = "squeezeLeft"
+        case SqueezeRight = "squeezeRight"
+        case SqueezeDown = "squeezeDown"
+        case SqueezeUp = "squeezeUp"
+        case FadeIn = "fadeIn"
+        case FadeOut = "fadeOut"
+        case FadeOutIn = "fadeOutIn"
+        case FadeInLeft = "fadeInLeft"
+        case FadeInRight = "fadeInRight"
+        case FadeInDown = "fadeInDown"
+        case FadeInUp = "fadeInUp"
+        case ZoomIn = "zoomIn"
+        case ZoomOut = "zoomOut"
+        case Fall = "fall"
+        case Shake = "shake"
+        case Pop = "pop"
+        case FlipX = "flipX"
+        case FlipY = "flipY"
+        case Morph = "morph"
+        case Squeeze = "squeeze"
+        case Flash = "flash"
+        case Wobble = "wobble"
+        case Swing = "swing"
+    }
+
     func animatePreset() {
         alpha = 0.99
         if animation == "" {
