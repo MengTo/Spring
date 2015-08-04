f5a87c2901466d358c552f62ec5a11dfb03ad862
André Schneider <mail@schneiderandre.com>
2015-08-04 15:48:22 +0200

    Remove top level functions.

--- Spring/TransitionManager.swift
+++ Spring/TransitionManager.swift
@@ -37,7 +37,7 @@ public class TransitionManager: NSObject, UIViewControllerTransitioningDelegate,
             toView.transform = CGAffineTransformMakeTranslation(0, container.frame.size.height)
             container.addSubview(fromView)
             container.addSubview(toView)
-            springEaseInOut(duration) {
+            SpringAnimation.springEaseInOut(duration) {
                 fromView.transform = CGAffineTransformMakeScale(0.8, 0.8)
                 fromView.alpha = 0.5
                 toView.transform = CGAffineTransformIdentity
@@ -58,7 +58,7 @@ public class TransitionManager: NSObject, UIViewControllerTransitioningDelegate,
             container.addSubview(toView)
             container.addSubview(fromView)
 
-            springEaseInOut(duration) {
+            SpringAnimation.springEaseInOut(duration) {
                 fromView.transform = CGAffineTransformMakeTranslation(0, fromView.frame.size.height)
                 toView.transform = CGAffineTransformIdentity
                 toView.alpha = 1
