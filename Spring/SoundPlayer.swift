c2b2fb889e2b643d41dd628d7fe885186fbe985d
Meng To <shadownessguy@gmail.com>
2015-08-23 17:32:59 -0400

    Fix Soundplayer

--- Spring/SoundPlayer.swift
+++ Spring/SoundPlayer.swift
@@ -23,40 +23,38 @@
 import UIKit
 import AudioToolbox
 
-public class SoundPlayer: NSObject {
-
-    @IBInspectable var filename : String?
-    @IBInspectable var enabled : Bool = true
-
+struct SoundPlayer {
+    
+    static var filename : String?
+    static var enabled : Bool = true
+    
     private struct Internal {
         static var cache = [NSURL:SystemSoundID]()
     }
-
-    public func playSound(soundFile:String) {
-
+    
+    static func playSound(soundFile: String) {
+        
         if !enabled {
             return
         }
-
+        
         if let url = NSBundle.mainBundle().URLForResource(soundFile, withExtension: nil) {
-
+            
             var soundID : SystemSoundID = Internal.cache[url] ?? 0
-
+            
             if soundID == 0 {
                 AudioServicesCreateSystemSoundID(url, &soundID)
                 Internal.cache[url] = soundID
             }
-
+            
             AudioServicesPlaySystemSound(soundID)
-
+            
         } else {
             println("Could not find sound file name `\(soundFile)`")
         }
     }
-
-    @IBAction public func play(sender: AnyObject?) {
-        if let filename = filename {
-            self.playSound(filename)
-        }
+    
+    static func play(file: String) {
+        self.playSound(file)
     }
 }
