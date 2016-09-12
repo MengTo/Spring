25fcf662ce33a208b76c7ee5cf75254bb79896f6
Rishabh Tayal <rtayal11@gmail.com>
2016-09-12 12:27:09 -0500

    warning fix

--- SpringApp/AppDelegate.swift
+++ SpringApp/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
