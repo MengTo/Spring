56da74db2ec99a0bd064b461836d849433de7610
Alexey Efimov <lex.efimov@gmail.com>
2022-04-12 15:19:56 +0300

    Create platform targer

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Spring",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
