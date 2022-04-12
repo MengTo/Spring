bea1940f08abb8cc47f7a7a420f36876d3c35c1d
Alexey Efimov <lex.efimov@gmail.com>
2022-04-12 13:31:16 +0300

    Create Package.swift manifest

---
+++ Spring/Sources/Spring/Spring.swift
@@ -0,0 +1,6 @@
+public struct Spring {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Spring/Tests/SpringTests/SpringTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Spring
+
+final class SpringTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Spring().text, "Hello, World!")
+    }
+}
